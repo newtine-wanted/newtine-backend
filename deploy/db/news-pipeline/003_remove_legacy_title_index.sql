3104f466f53a40d73ea68bf5a3c2ca34bfb27f9f
강서현 <shyn.kxxg@gmail.com>
2026-09-20 16:07:56 +0900

    chore: remove local-only SQL cleanup and renumber search seed

--- deploy/db/news-pipeline/003_remove_legacy_title_index.sql
+++
@@ -1,3 +0,0 @@
--- Optional cleanup only for databases that applied the earlier pipeline DDL.
--- No table data is changed. Fresh installations do not need this file.
-drop index if exists news_collection_published_title_trgm;
